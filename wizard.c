5c11253dc52115a675e4f26c7b767d806e52ba8a
Takey Tajwar <tajwar.earth@pm.me>
2023-03-08 15:53:45 +0600

    remove <> around keybind

--- wizard.c
+++ wizard.c
@@ -21,17 +21,17 @@ void tasks_navigation_help()
 	clear_help();
 
 	wattron(wiz_win,   COLOR_PAIR(3));
-	print_help(3, "<%c> or <%c> to go to the next line", KB_NEXT, AKB_NEXT);
-	print_help(4, "<%c> or <%c> to go to the previous line", KB_PREV, AKB_PREV);
-	print_help(5, "<%c> or <%c> to quit", KB_QUIT, AKB_QUIT);
-	print_help(6, "<%c> to go to the start of the window", KB_START);
-	print_help(7, "<%c> to go to the end of the window", KB_END);
-	print_help(8, "<%c> to go to the center of the window", KB_CENTER);
-	print_help(9, "<%c> to switch between windows", KB_WSWITCH);
-	print_help(10, "<%c> to add a new task", KB_ADD_NEW_TASK);
-	print_help(11, "<%c> to mark the task done", KB_DONE);
-	print_help(12, "<%c> to skip the task", KB_SKIP);
-	print_help(13, "<%c> to delete the task", KB_DELETE);
+	print_help(3, "%c or %c to go to the next line", KB_NEXT, AKB_NEXT);
+	print_help(4, "%c or %c to go to the previous line", KB_PREV, AKB_PREV);
+	print_help(5, "%c or %c to quit", KB_QUIT, AKB_QUIT);
+	print_help(6, "%c to go to the start of the window", KB_START);
+	print_help(7, "%c to go to the end of the window", KB_END);
+	print_help(8, "%c to go to the center of the window", KB_CENTER);
+	print_help(9, "%c to switch between windows", KB_WSWITCH);
+	print_help(10, "%c to add a new task", KB_ADD_NEW_TASK);
+	print_help(11, "%c to mark the task done", KB_DONE);
+	print_help(12, "%c to skip the task", KB_SKIP);
+	print_help(13, "%c to delete the task", KB_DELETE);
 
 	wrefresh(wiz_win);
 }
@@ -52,7 +52,7 @@ void warn_nokb(int format)
 {
 	clear_warn();
 	wattron(wiz_win,   COLOR_PAIR(5));
-	print_help(getmaxy(wiz_win)-4, "No keybinding for <%c>", format);
+	print_help(getmaxy(wiz_win)-4, "No keybinding for %c", format);
 	wattroff(wiz_win,   COLOR_PAIR(5));
 	wrefresh(wiz_win);
 }
