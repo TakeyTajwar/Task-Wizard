1840971b323196c675720294b1a2071b5475078b
Takey Tajwar <tajwar.earth@pm.me>
2023-03-11 16:47:29 +0600

    add config for date-time separators and recurring factors

--- config.h
+++ config.h
@@ -7,4 +7,15 @@
 #define DATA_DIR	"/.local/share/Twiz/"
 #define DB_FILE		"Task_Wizard.db"
 
+// Date Time
+
+#define DATE_SEP	'-'
+#define TIME_SEP	':'
+
+// Recurring Factors
+#define DAILY		'd'
+#define WEEKLY		'w'
+#define MONTHLY		'm'
+#define YEARLY		'y'
+
 #endif
